f525893bf936d2690bd0dd247fcf1f511c30f2cc
Rijan Chapgain <hrijon.chp@gmail.com>
2019-08-29 12:49:18 +0800

    Displaying javascript student result in html table

--- q2Script.js
+++ q2Script.js
@@ -1,32 +1,75 @@
-
-var x = 0;
-var y = 0;
-var text = "";
-var array = Array();
-var i;
+var x =0;
+var y =0;
+var studentNames = [];
+var studentMarks = [];
 
 function add_element_to_array()
 {
-    array[x] = document.getElementById("text1").value;
-    array[y] = document.getElementById("text2").value;
-
-    alert("Element: " + array[x] + "&" + array[y] + " Added at index " + x + "&" +y);
-    x++;
-    document.getElementById("text1").value = "";
-    document.getElementById("text2").value = "";
-
-    display_array();
+    add_name();
+    add_marks();   
 }
 
-function display_array()
-  {
-    var e = "";   
-        
-    for (var z=0; z<array[x,y].length; z++)
-    {
-      e += z +  ") " + array[z] + "\n\n";
+function add_name()
+{
+    var text1 = document.getElementById("text1").value;
+    studentNames.push(text1);
+    x++;
+    document.getElementById("text1").value = "";
 
+    document.getElementById("Names").innerHTML =studentNames;
+}
+
+function add_marks()
+{
+    var text2 = document.getElementById("text2").value;
+    studentMarks.push(text2);
+    y++;
+    document.getElementById("text2").value = "";
+
+    document.getElementById("Marks").innerHTML = studentMarks;
+
+    if (studentMarks >= 80)
+    {
+        document.getElementById("grade").innerHTML = "H.D";
+        document.getElementById("percent").innerHTML = studentMarks + "%";
+    }
+
+    if (70 <= studentMarks < 80)
+    {
+        document.getElementById("grade").innerHTML = "D";
+        document.getElementById("percent").innerHTML = studentMarks + "%";
+    }
+
+    if (60 <= studentMarks < 70)
+    {
+        document.getElementById("grade").innerHTML = "Cr";
+        document.getElementById("percent").innerHTML = studentMarks + "%";
     }
     
-    document.getElementById("Result").innerHTML = e;
-  }
+    if (50 <= studentMarks <60)
+    {
+        document.getElementById("grade").innerHTML = "P";
+        document.getElementById("percent").innerHTML = studentMarks + "%";
+    }
+    
+    if (studentMarks <50)
+    {
+        document.getElementById("grade").innerHTML = "N";
+        document.getElementById("percent").innerHTML = studentMarks + "%";
+    }
+    
+    
+}
+
+function table_heading()
+{
+    // get table element
+    var myTable = document.getElementById("myTable");
+    // get the th elements    
+    var ths = myTable.getElementsByTagName("th");
+}
+
+// function grade()
+// {
+
+// }
